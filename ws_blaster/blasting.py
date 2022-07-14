a9a257170a3d34c6cbd9bfc079edde96b7edfa9d
AhilanAshwin <ahilanaashwin@gmail.com>
2022-07-14 15:58:15 +0800

    Created blaster class

--- ws_blaster/blasting.py
+++ ws_blaster/blasting.py
@@ -1,1 +1,55 @@
+import re
+import random
+import pandas as pd
 from ws_blaster.utils import open_driver
+
+
+class Blaster:
+    def __init__(self):
+        pass
+
+    @property
+    def columns(self):
+        if isinstance(self.contacts_df, pd.DataFrame):
+            return self.contact_numbers.columns
+        return "No DataFrame found"
+    
+    @property
+    def contact_numbers_info(self):
+        info_dict = {
+            "len_phone_numbers":len(set(self.contact_numbers)),
+            "sample_of_5": random.sample(self.contact_numbers, 5)
+        }
+        return info_dict
+
+    @staticmethod
+    def clean_numbers(df: pd.DataFrame, col: str) -> list:
+        '''
+        Clean numbers to required format for whatsapp search
+
+        df: Dataframe [pandas dataframe]
+        col: Column name containing the numbers to blast [str]
+        
+        returns dataframe with cleaned numbers
+        '''
+        df[col] = df[col].astype(str)
+        df[col] = [re.sub("[^0-9]", "", x) for x in df[col]]
+        df = df[df[col] != '']
+        df[col] = ['60' + x if (x[0] == '1' and 8 < len(x) < 11) else x for x in df[col]]
+        df[col] = ['6' + x if (x[0] == '0' and 9 < len(x) < 12) else x for x in df[col]]
+        df[col] = ['' if (x[2] != '1' or len(x) > 12 or len(x) < 11) else x for x in df[col]]
+        df = df[df[col] != '']
+        df = df.drop_duplicates(subset = col)
+        contact_numbers = df[col].to_list()
+        return contact_numbers
+
+    def extract_from_csv(self, file, phone_number_column):
+        self.contacts_df = pd.read_csv(file)
+        self.contact_numbers = self.clean_numbers(self.contacts_df, phone_number_column)
+
+
+
+
+
+    
+
