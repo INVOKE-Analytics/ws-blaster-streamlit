0df0a071bec1f7f450f936002f2e486d818fa30b
AhilanAshwin <ahilanaashwin@gmail.com>
2022-07-15 11:00:38 +0800

    pytest works

--- tests/test_blasting.py
+++ tests/test_blasting.py
@@ -5,5 +5,3 @@ def test_extract_from_file():
     blaster = Blaster()
     numbers = blaster.extract_from_file("tests/Sample Phone.csv")
     assert isinstance(numbers, pd.DataFrame)
-
-test_extract_from_file()
