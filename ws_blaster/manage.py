efb569870963214767066e0899fc50b8b0f1b64b
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-15 10:13:05 +0800

    add new function

--- ws_blaster/manage.py
+++ ws_blaster/manage.py
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f5e7fdac862344da79727543668de8366e7027b8
 def opt3():
     option3 = st.selectbox('Select set of accounts to check', ('', 'meniaga','AyuhMalaysia','Burner Accounts'))
     if option3 != '':
         if option3 == 'Burner Accounts':
             option3 = 'burner'
-<<<<<<< HEAD
             return option3
-=======
-print("Hello world!")
-
-x = "Ammar"
->>>>>>> ff2f54adc2ceb880e765d71f6f9d862e82c135dd
-=======
-            return option3
->>>>>>> f5e7fdac862344da79727543668de8366e7027b8
+            
