f0a5248d7b650afeedc37c342cd538a96fb7eda7
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-15 09:40:28 +0800

    add function opt3

--- ws_blaster/manage.py
+++ ws_blaster/manage.py
@@ -1,5 +1,6 @@
-print("Hello world!")
-
-x = "Ammar"
-
-print('hello')
+def opt3():
+    option3 = st.selectbox('Select set of accounts to check', ('', 'meniaga','AyuhMalaysia','Burner Accounts'))
+    if option3 != '':
+        if option3 == 'Burner Accounts':
+            option3 = 'burner'
+            return option3
