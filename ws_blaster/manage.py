74d035f54455d63789f2bca2b42e6f627bb9addb
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-15 12:15:50 +0800

    add main_option_2 function

--- ws_blaster/manage.py
+++ ws_blaster/manage.py
@@ -72,4 +72,44 @@ def delete_unav_account(not_available):
         for n in not_available:
             path_delete = mypath + n
             shutil.rmtree(path_delete)
-        st.subheader('Succesfully deleted unavailable account(s)!')          
+        st.subheader('Succesfully deleted unavailable account(s)!')
+
+def main_option_2(option2):
+        if option2 == 'Check available account(s)':
+            option3 = opt3()
+            
+
+            accs = remove_DS_store(option3)
+            with st.spinner('Checking Accounts...'):
+                available = account_collection(accs, option3)[0]
+                not_available = account_collection(accs, option3)[1]
+                
+            checking_avb_account = checking_acc_availability(available, not_available)
+
+            return checking_avb_account
+
+        elif option2 == 'Add new account(s)':
+            option3 = opt3()
+            accs = remove_DS_store(option3)
+
+            name = st.text_area("Enter Whatsapp account name:")
+            name = name.split(',')
+            name = [x.strip() for x in name]
+            taken = [x for x in name if x in accs]
+            check_new_account = add_new_acc(taken, option3, name)
+
+            return check_new_account
+    
+        elif option2 == 'Delete unavailable account(s)':
+            option3 = opt3()
+            accs = remove_DS_store(option3)
+
+            st.subheader('Accounts: ' + ', '.join(accs))
+
+            with st.spinner('Deleting Accounts...'):
+                available = checking_acc(accs, option3)[0]
+                not_available = checking_acc(accs, option3)[1]
+
+                # here
+                delete_not_av = delete_unav_account(not_available)
+                return delete_not_av          
