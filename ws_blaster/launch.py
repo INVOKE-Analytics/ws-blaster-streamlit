52419d5824d0ba65d4c114d9d0f95f9ef4e39639
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-18 15:09:29 +0800

    Remove the function

--- ws_blaster/launch.py
+++ ws_blaster/launch.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from ws_blaster.manage import Manage
 
 # Hide streamlit header and footer
 hide_st_style = """
@@ -10,20 +11,4 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-def subheader(word):
-    return st.subheader(word)
 
-def button(word):
-    return st.button(word)
-
-def spinner(word):
-    return st.spinner(word)
-
-def write(word):
-    return st.write(word)
-
-def selectbox(word):
-    return st.selectbox(word)
-
-def image(word):
-    return st.image(word)
