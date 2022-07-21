5a58b934d38493ed6c22c3ce74009ffeb7b3ff89
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-21 22:16:51 +0800

    the most updated update anc function cleaning

--- ws_blaster/manage.py
+++ ws_blaster/manage.py
@@ -9,28 +9,27 @@ import shutil
 import pathlib
 
 class Manage:
-    def __init__(self, user_path, platform):
+    def __init__(self, user_path):
         self.user_path = pathlib.Path(user_path)
-        self.platform = platform
         self.driver_dict = {}
         self.account_dict = {}
 
-    def get_name(self,name):
+    def get_name(self, name):
         """
         Return list of name
         """
-        self.name = name.split(',') 
+        name = name.split(',') 
         name = [x.strip() for x in name]
         return name
     
     def get_all_account_list_dir(self):
-        mypath = 'user-data-dir=' + self.user_path + '/' 
-        accs = [f for f in listdir(mypath)]
+        path_to_user = self.user_path + '/' 
+        accs = [f for f in listdir(path_to_user)]
         return accs
 
-    def checking_account(self):
+    def checking_account(self,platform):
         accs = self.get_all_account_list_dir(self)
-        path_to_platform = 'user-data-dir=' + self.user_path + '/' + self.platform + '/'
+        path_to_platform = 'user-data-dir=' + self.user_path + '/' + str(platform) + '/'
 
         available = [] # if len > 1, there is account
         not_available = []
@@ -45,29 +44,30 @@ class Manage:
         return (available, not_available)
     
         
-    def setup_new_account(self, account_name):
+    def setup_new_account(self, platform, account_name):
         """
         Component 1 for 'Add account'
         """
-        path_to_account = 'user-data-dir=' + self.user_path + '/' + self.platform + '/' + account_name
+        path_to_account = 'user-data-dir=' + self.user_path + '/' + str(platform) + '/' + account_name
         driver = open_driver(path_to_account, headless = False)
         self.account_dict[account_name] = driver
     
 
-    def automatically_deleted_account_if_error(self):
+    def automatically_deleted_account_if_error(self, platform, name):
         """
         Component 2 for 'Add account'
         """
-        n = self.get_name(self)
-        mypath = 'user-data-dir=' + self.user_path + '/' + self.platform + '/' + n
-        return shutil.rmtree(mypath) 
+        account_name = self.get_name(self, name)
+        path_to_acount = 'user-data-dir=' + self.user_path + '/' + str(platform) + '/' + account_name
+        shutil.rmtree(path_to_acount)
+        self.account_dict[account_name] = 'deleted'
 
-    def get_taken(self):
+    def get_taken(self, name):
         """
         Return name_item within name list, that only exist in accs
         """
-        name = self.get_name(self)
-        accs = self.get_accs(self)
+        name = self.get_name(self, name)
+        accs = self.get_all_account_list_dir(self)
         taken = [x for x in name if x in accs]
         return taken
 
@@ -78,9 +78,10 @@ class Manage:
         option1 = 'Account Management'
         return option1
 
+    @property
     def ws_logo(self):
         path = '/Users/amerwafiy/Desktop/ws-blasting/ws-logo.png'
         return path
 
-    def select_box_option1_acc_management(self):
+    def get_option_for_account_management(self):
         return  ['Add new account(s)','Check available account(s)', 'Delete unavailable account(s)']
