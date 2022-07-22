0afcfca0f9cfdd066109be1677f14572269bc70d
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-22 17:01:26 +0800

    Updated new test for test_manage.py. The test_automatically function still failed

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -1,3 +1,53 @@
+from requests import head
+from ws_blaster.utils import open_driver
 from ws_blaster.manage import Manage
 
+manage = Manage(user_path='D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users')
 
+def test_get_name():
+    assert manage.get_name("Sharafi, Anis") == ['Sharafi', 'Anis']
+
+def test_get_all_platform_list_dir():
+    test_fx = manage.get_all_platform_list_dir()
+    exp_output = ['AyuhMalaysia', 'burner', 'meniaga']
+    assert test_fx == exp_output
+
+def test_get_all_account_name():
+    test_fx = manage.get_all_account_name("meniaga")
+    exp_output = ['Ammar', 'sharafi']
+    assert test_fx == exp_output
+
+def test_checking_account_list_dir():
+    get_acc = manage.checking_account_list_dir("meniaga")
+    test_av = []
+    test_not_av = ['D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users\\meniaga\\Ammar',
+        'D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users\\meniaga\\sharafi']
+    assert get_acc[0] == test_av
+    assert get_acc[1] == test_not_av
+
+def test_create_new_user_file():
+    manage.create_new_user_file('meniaga', 'Ammar')
+    assert len(manage.driver_dict) == 1
+
+def test_setup_path_new_account():
+    add_new_account = manage.get_path_new_account("meniaga", "Ammar")
+    assert add_new_account == 'user-data-dir=D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users\\meniaga\\Ammar'
+
+
+def test_automatically_deleted_account_if_error():
+    # TODO: Test still FAILED
+    manage.create_new_user_file('meniaga', 'Ammar')
+    manage.automatically_deleted_account_if_error('meniaga', 'Ammar')
+    assert len(manage.account_dict) == 1
+
+def test_taken():
+    test_input = manage.get_taken('sharafi','meniaga')
+    assert test_input == ['sharafi']
+
+    
+def test_get_option1():
+    test_input = 'Account Management'
+    assert test_input == 'Account Management'
+
+def get_option_for_account_management():
+    assert isinstance(manage.get_option_for_account_management, list)
