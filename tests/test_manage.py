9132fc0817ae86419c46430daa125cff3cdbc7a7
Ammar Azman <muhammarsafuan@gmail.com>
2022-07-26 09:51:57 +0800

    change the test_checking_account_list_dir to test_checking_account_banned_or_not

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -9,14 +9,13 @@ def test_get_name():
 
 def test_get_all_account_name():
     test_fx = manage.get_all_account_name("meniaga")
-    exp_output = ['Ammar', 'sharafi']
+    exp_output = ['Ahilan','Ammar']
     assert test_fx == exp_output
 
-def test_checking_account_list_dir():
-    get_acc = manage.checking_account_list_dir("meniaga")
+def test_checking_banned_or_not():
+    get_acc = manage.checking_banned_or_not("meniaga")
     test_av = []
-    test_not_av = ['D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users\\meniaga\\Ammar',
-        'D:\\Desktop\\INVOKE\\ws_blaster\\ahilan-branch\\venvAhilan\\ws-blaster-prod\\Users\\meniaga\\sharafi']
+    test_not_av = ['Ammar', 'Ahilan']
     assert get_acc[0] == test_av
     assert get_acc[1] == test_not_av
 
@@ -33,6 +32,10 @@ def test_automatically_deleted_account_if_error():
 def test_taken():
     test_input = manage.get_taken('sharafi','meniaga')
     assert test_input == ['sharafi']
+
+def test_take_screenshot():
+    manage.take_screenshot()
+    assert len(manage.take_screenshot) == 1
 
     
 
