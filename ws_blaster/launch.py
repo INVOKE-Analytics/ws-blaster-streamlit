d2570e008a001bff0c6da006dd0e0e92608b1894
Ammar Azman <muhammarsafuan@gmail.com>
2022-08-09 11:53:15 +0800

    update the code

--- ws_blaster/launch.py
+++ ws_blaster/launch.py
@@ -36,57 +36,57 @@ option1 = st.sidebar.selectbox('Select option', ('Blast Messages', 'Account Mana
 ##############################################################
 # Blasting
 ##############################################################
-blaster = Blaster(user_path="./Users")
-if option1 == 'Blast Messages':
-    st.image("images/whats-app-img.png")
+def main_blasting():
+        blaster = Blaster(user_path="./Users")
+        st.image("images/whats-app-img.png")
 
-    # Upload csv file to extract phone numbers
-    uploaded_file = st.file_uploader(
-        "Choose a file with phone numbers (csv)", type='csv')
-    if uploaded_file is not None:
-        blaster.extract_from_file(uploaded_file)
-        col = st.selectbox('Select phone number column', blaster.columns)
-        numbers = blaster.clean_numbers(col)
-        st.json(blaster.contact_numbers_info)
+        # Upload csv file to extract phone numbers
+        uploaded_file = st.file_uploader(
+            "Choose a file with phone numbers (csv)", type='csv')
+        if uploaded_file is not None:
+            blaster.extract_from_file(uploaded_file)
+            col = st.selectbox('Select phone number column', blaster.columns)
+            numbers = blaster.clean_numbers(col)
+            st.json(blaster.contact_numbers_info)
 
-    # Upload pictures
-    if uploaded_file:
-        uploaded_imgs = st.file_uploader(
-            "Choose imgs you want to blast (png)", type='png', accept_multiple_files=True)
-        blaster.save_files_to_blast(uploaded_imgs)
+        # Upload pictures
+        if uploaded_file:
+            uploaded_imgs = st.file_uploader(
+                "Choose imgs you want to blast (png)", type='png', accept_multiple_files=True)
+            blaster.save_files_to_blast(uploaded_imgs)
 
-    # Variations of the same message
-    if uploaded_file:
-        message_variations = st.select_slider(
-            "How many variations of the same message do you have?", options=list(range(1, 5)))
-        for i in range(message_variations):
-            message = st.text_area("Enter message: ", key=i)
-            blaster.add_message_variations_to_blast(message)
+        # Variations of the same message
+        if uploaded_file:
+            message_variations = st.select_slider(
+                "How many variations of the same message do you have?", options=list(range(1, 5)))
+            for i in range(message_variations):
+                message = st.text_area("Enter message: ", key=i)
+                blaster.add_message_variations_to_blast(message)
 
-    # Choose the set of platform to blast from
-    if uploaded_file:
-        platform = st.selectbox("Choose set of accounts to blast from",
-                                ('Select', 'meniaga', 'AyuhMalaysia', "Burner Accounts"))
+        # Choose the set of platform to blast from
+        if uploaded_file:
+            platform = st.selectbox("Choose set of accounts to blast from",
+                                    ('Select', 'meniaga', 'AyuhMalaysia', "Burner Accounts"))
 
-    # Start Blasting
-    # TODO: Check if unavailable
-    if uploaded_file and platform != "Select":
-        start = st.button('Start Blasting')
-        percent_complete = 0
-        if start:
-            st.info("Setting up Web Drivers")
-            blaster.setup_drivers_in_account(platform)
-            my_progress = st.progress(0.0)
-            for i, number in enumerate(numbers):
-                acc, driver = blaster.nav_to_number(number)
-                message = blaster.get_random_message()
-                file_path = str(blaster.imgs)
-                status = blaster.send_file(driver, file_path)
-                status = blaster.send_message(driver, message)
-                my_progress.progress((i+1)/len(numbers))
-                blaster.apply_random_wait(i)
-            st.success("Messages sent to all numbers")
-            blaster.close_drivers()
+        # Start Blasting
+        # TODO: Check if unavailable
+        if uploaded_file and platform != "Select":
+            start = st.button('Start Blasting')
+            percent_complete = 0
+            if start:
+                st.info("Setting up Web Drivers")
+                blaster.setup_drivers_in_account(platform)
+                my_progress = st.progress(0.0)
+                for i, number in enumerate(numbers):
+                    acc, driver = blaster.nav_to_number(number)
+                    message = blaster.get_random_message()
+                    file_path = str(blaster.imgs)
+                    status = blaster.send_file(driver, file_path)
+                    status = blaster.send_message(driver, message)
+                    my_progress.progress((i+1)/len(numbers))
+                    blaster.apply_random_wait(i)
+                st.success("Messages sent to all numbers")
+                blaster.close_drivers()
 
 
 ##############################################################
@@ -385,7 +385,9 @@ def main_account_management():
 
 
 if __name__ == '__main__':
-    if option1 == 'Account Management':
+    if option1 == 'Blast Messages':
+        main_blasting()
+    elif option1 == 'Account Management':
         main_account_management()
 
 
