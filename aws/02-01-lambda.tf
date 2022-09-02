a0e038f54a028c81b63d681ef1bda94a0c1e809a
AhilanAshwin <ahilanaashwin@gmail.com>
2022-09-02 15:20:10 +0800

    cannot create lambda

--- aws/02-01-lambda.tf
+++ aws/02-01-lambda.tf
@@ -26,57 +26,70 @@
 #   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 # }
 
-# resource "aws_lambda_function_event_invoke_config" "lambda" {
-#   for_each      = toset(local.environments)
-#   function_name = aws_lambda_function.api_lambda[each.key].function_name
-#   destination_config {
-#     on_success {
-#       destination = module.sqs[each.key].sqs_queue_arn
-#     }
-#     on_failure {
-#       destination = module.sqs[each.key].sqs_queue_arn
-#     }
-#   }
-# }
+data "aws_iam_role" "lambda_role" {
+  name = "WhatsAppBlasterLambdaRole"
+}
 
-# resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-#   for_each         = toset(local.environments)
-#   event_source_arn = module.sqs[each.key].sqs_queue_arn
-#   function_name    = aws_lambda_function.worker_lambda[each.key].arn
-# }
+data "aws_ecr_image" "lambda_image" {
+  repository_name = "whatsapp-blaster"
+  image_tag       = "latest"
+}
 
-# resource "aws_lambda_permission" "api_lambda_permission" {
-#   for_each      = toset(local.environments)
-#   statement_id  = "AllowExecutionFromAPIGateway"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.api_lambda[each.key].function_name
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
-# }
+data "aws_ecr_repository" "ecr_repo" {
+  name = "whatsapp-blaster"
+}
 
-# resource "aws_lambda_function" "api_lambda" {
-#   for_each      = toset(local.environments)
-#   function_name = "${local.prefix}-api-${each.key}"
-#   role          = aws_iam_role.lambda_role.arn
-#   image_uri     = "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
-#   package_type  = "Image"
-#   environment {
-#     variables = {
-#       "STAGE"   = "${each.key}",
-#       "SQS_URL" = "${local.prefix}-sqs-${each.key}"
+resource "aws_lambda_function_event_invoke_config" "lambda" {
+  for_each      = toset(local.environments)
+  function_name = aws_lambda_function.api_lambda[each.key].function_name
+  destination_config {
+    on_success {
+      destination = module.sqs[each.key].sqs_queue_arn
+    }
+    on_failure {
+      destination = module.sqs[each.key].sqs_queue_arn
+    }
+  }
+}
 
-#     }
-#   }
-# }
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  for_each         = toset(local.environments)
+  event_source_arn = module.sqs[each.key].sqs_queue_arn
+  function_name    = aws_lambda_function.worker_lambda[each.key].arn
+}
 
-# resource "aws_lambda_function" "worker_lambda" {
-#   for_each      = toset(local.environments)
-#   function_name = "${local.prefix}-worker-${each.key}"
-#   role          = aws_iam_role.lambda_role.arn
-#   image_uri     = "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
-#   package_type  = "Image"
+resource "aws_lambda_permission" "api_lambda_permission" {
+  for_each      = toset(local.environments)
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda[each.key].function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+}
 
-#   image_config {
-#     command = ["app.lambda_function.consumer_handler"]
-#   }
-# }
+resource "aws_lambda_function" "api_lambda" {
+  for_each      = toset(local.environments)
+  function_name = "${local.prefix}-api-${each.key}"
+  role          = data.aws_iam_role.lambda_role.arn
+  image_uri     = "${data.aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type  = "Image"
+  environment {
+    variables = {
+      "STAGE"   = "${each.key}",
+      "SQS_URL" = "${local.prefix}-sqs-${each.key}"
+
+    }
+  }
+}
+
+resource "aws_lambda_function" "worker_lambda" {
+  for_each      = toset(local.environments)
+  function_name = "${local.prefix}-worker-${each.key}"
+  role          = data.aws_iam_role.lambda_role.arn
+  image_uri     = "${data.aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type  = "Image"
+
+  image_config {
+    command = ["app.lambda_function.consumer_handler"]
+  }
+}
