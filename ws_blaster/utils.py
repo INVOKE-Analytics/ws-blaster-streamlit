f0d35aef5d68bb1ad9e84bedd71a7bbf1d99a61e
Ammar-Azman <muhammarsafuan@gmail.com>
2022-09-09 15:36:13 +0800

    upload chromedriver

--- ws_blaster/utils.py
+++ ws_blaster/utils.py
@@ -64,6 +64,31 @@ def open_driver_blasting(user_path):
     driver.execute_script("window.onbeforeunload = function() {};")
     return driver, display
 
+def open_driver_Streamlit(user_path, headless=True):
+    '''
+    Opens chromedriver and initialize Whatsapp web
+
+    user_path: Path where user credentials are located
+    headless: Decides whether to run on headless mode or otherwise
+
+    Returns a chromedriver instance
+    '''
+    chrome_options = webdriver.Chrome('./ws-blaster-prod-f1/ws_blaster/chromedriver/chromedriver.exe')
+    chrome_options.add_argument(user_path)
+    chrome_options.add_argument('--disable-notifications')
+
+    if headless:
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920,1080")
+        # Specify user-agent to allow headless mode
+        chrome_options.add_argument(
+            "user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
+    driver.get('https://web.whatsapp.com/')
+    driver.execute_script("window.onbeforeunload = function() {};")
+    return driver
 # from selenium.webdriver.common.by import By
 # driver, display = open_driver_beta("user-data-dir=Users/AyuhMalaysia/Fizah")
 # driver.get_screenshot_as_file("screenshot.png")
