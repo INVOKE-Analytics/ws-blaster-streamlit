9a28c2e5445569fa53a4a928d0d4dac7d46469b1
Ammar-Azman <muhammarsafuan@gmail.com>
2022-09-09 10:32:57 +0800

    update mainAPI

--- app/mainAPI.py
+++ app/mainAPI.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 import app.routers.blastingAPI as blastingAPI
 import app.routers.manageAPI as manageAPI
+from mangum import Mangum
 
-app = FastAPI(title="Whatsapp Blaster API", 
-            description="An API to blast message using Whatsapp.", 
-            version="0.1.0")
+from app.config import get_settings
+
+settings = get_settings()
+root_path = "/" if settings.STAGE == 'local' else f"/{settings.STAGE}/"
+app = FastAPI(title=f"{settings.PROJECT_NAME} ({settings.STAGE.upper()})",
+              root_path=root_path,
+              description="""An API Service created to blast promotional 
+                            messages to prospective clients using Whats App.""")
+handler = Mangum(app)
+
+
+
 app.include_router(blastingAPI.router)
 app.include_router(manageAPI.router)
 
